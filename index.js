9c1f73fb9519edbf1bd29044be26f9f61bec0c27
Nazar <casablancaplusplus@gmail.com>
2016-02-11 03:28:38 +0330

    initial commit

--- index.js
+++ index.js
@@ -13,8 +13,10 @@ if (!databaseUri) {
 var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '' //Add your master key here. Keep it secret!
+  appId: process.env.APP_ID || 'b46645fb-592c-4b1f-8f66-f059ea2c3d17',
+  masterKey: process.env.MASTER_KEY || 'nazar', //Add your master key here. Keep it secret!
+  clientKey : 'android',
+  restAPIKey : 'restKey'
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
